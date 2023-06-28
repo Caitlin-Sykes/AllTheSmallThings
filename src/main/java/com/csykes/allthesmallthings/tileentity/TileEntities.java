45fc9fa6cba718ea11e0a0e3c05133f597885c5b
Cait <2423179@dundee.ac.uk>
2023-06-28 20:22:18 +0100

    Added sawblade item

--- src/main/java/com/csykes/allthesmallthings/tileentity/TileEntities.java
+++ src/main/java/com/csykes/allthesmallthings/tileentity/TileEntities.java
@@ -12,6 +12,7 @@ public class TileEntities {
 
     public static DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, AllTheSmallThings.MOD_ID);
 
+    //Registers Tiles
     public static RegistryObject<TileEntityType<DebarkerTile>> DEBARKER_TILE = TILE_ENTITIES.register("debarker_tile", () -> TileEntityType.Builder.create(DebarkerTile::new, ModBlocks.DEBARKER.get()).build(null));
 
     public static void register(IEventBus eventBus) {
