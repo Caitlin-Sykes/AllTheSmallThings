eb914222b9c4448775ea83f2cc6c70c057bcc1cc
Cait <2423179@dundee.ac.uk>
2023-08-08 22:18:21 +0100

    revamped code

--- src/main/java/com/csykes/allthesmallthings/tileentity/DebarkerTile.java
+++ src/main/java/com/csykes/allthesmallthings/tileentity/DebarkerTile.java
@@ -27,8 +27,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class DebarkerTile extends TileEntity {
     private final ItemStackHandler itemHandler = createHandler();
     private final LazyOptional<IItemHandler> handler = LazyOptional.of(() -> itemHandler);
-    private final String REGEX = ":(.+)_";
-    private int lastSlotValue;
+    private static final String REGEX = ":(.+)_";
 
     public DebarkerTile(TileEntityType<?> tileEntityTypeIn) {
         super(tileEntityTypeIn);
@@ -38,50 +37,32 @@ public class DebarkerTile extends TileEntity {
         this(TileEntities.DEBARKER_TILE.get());
     }
 
-    // Getter
-    public int getLastSlotValue() {
-        return lastSlotValue;
-    }
 
-    // Setter
-    public void setLastSlotValue(int lastSlotValue) {
-        this.lastSlotValue = lastSlotValue;
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return handler.cast();
+        }
+        return super.getCapability(cap, side);
     }
 
     private ItemStackHandler createHandler() {
         return new ItemStackHandler(2) {
             @Override
-            /**
-             * Function for contents changing
-             * 
-             * @param slot - slot to check
-             */
             protected void onContentsChanged(int slot) {
-                markDirty();
-                System.out.println("onContentPingyPing");
-                if (slot == 0) {
-                    // If slot 0 is empty, slot 1 is also set to empty
-                    if (itemHandler.getStackInSlot(0).isEmpty()) {
-                        itemHandler.setStackInSlot(1, ItemStack.EMPTY);
-                    }
-
-                    else {
-                        stripLogs();
-                    }
+                if (slot == 0 && isItemValid(0, this.getStackInSlot(slot)) && this.getStackInSlot(0).getCount() != 0) {
+                    // convertLogToStripped();
                 }
+
+                markDirty();
             }
 
-            /**
-             * checks if item is valid
-             * 
-             * @param slot  - slot to check
-             * @param stack - item stack to check
-             */
             @Override
             public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-                switch (slot) {
+                 switch (slot) {
                     case 0:
-                        return (isLog(stack) == true && hasStrippedLog(stack) == true);
+                        return (isLog(stack) && hasStrippedLog(stack));
                     case 1:
                         return stack.isEmpty() || isStrippedLog(stack);
                     default:
@@ -90,177 +71,48 @@ public class DebarkerTile extends TileEntity {
                 }
             }
 
-            /**
-             * Limit of items in inventory slot
-             * Slot: inventory slot.
-             */
             @Override
             public int getSlotLimit(int slot) {
                 return 64;
             }
-
-            @Nonnull
-            @Override
-            /**
-             * Inserts an item
-             * 
-             * @param Slot      - slot to insert
-             * @param ItemStack - the item stack to enter
-             * @param simulate  - whether its been changed or not
-             */
-            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-                if (!isItemValid(slot, stack)) {
-                    return stack;
-                }
-
-                else {
-                    setLastSlotValue(itemHandler.getStackInSlot(0).getCount());
-                    return super.insertItem(slot, stack, simulate);
-                }
-            }
-
-            /**
-             * overrides, method extracts items from a slot
-             * 
-             * @param slot     - slot to copy
-             * @param amount   - amount to remove
-             * @param simulate - changes
-             */
-            @Nonnull
-            @Override
-            public ItemStack extractItem(int slot, int amount, boolean simulate) {
-
-                if (slot == 1 && !itemHandler.getStackInSlot(slot).isEmpty()) {
-                    ItemStack extractedStack = itemHandler.getStackInSlot(slot).copy();
-                    extractedStack.setCount(Math.min(amount, extractedStack.getCount()));
-                    if (!simulate) {
-                        itemHandler.getStackInSlot(slot).shrink(extractedStack.getCount());
-                        checkItemRemoved(slot, extractedStack.getCount());
-                    }
-                    return extractedStack;
-                } else if (slot == 0 && !itemHandler.getStackInSlot(slot).isEmpty()) {
-                    ItemStack extractedStack = itemHandler.getStackInSlot(slot).copy();
-                    extractedStack.setCount(Math.min(amount, extractedStack.getCount()));
-                    if (!simulate) {
-                        itemHandler.getStackInSlot(slot).shrink(extractedStack.getCount());
-                        checkItemRemoved(slot, extractedStack.getCount());
-
-                        // Update count of slot 1 to match slot 0
-                        itemHandler.getStackInSlot(1).setCount(itemHandler.getStackInSlot(0).getCount());
-                        markDirty(); // Mark the tile entity as dirty to save changes
-                    }
-                    return extractedStack;
-                }
-
-                return ItemStack.EMPTY;
-            }
-
         };
     }
 
-    @Nonnull
-    @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return handler.cast();
-        }
+    public void convertLogToStripped() {
+        try {
+            ItemStack inputStack = itemHandler.getStackInSlot(0);
+        if (!inputStack.isEmpty() && itemHandler.isItemValid(0, inputStack)) {
+            ItemStack strippedStack = getStrippedVersion(inputStack);
+            if (!strippedStack.isEmpty()) {
+                int inputCount = Math.min(inputStack.getCount(), 64); // Limit to 20 logs
+                ItemStack strippedLogs = new ItemStack(strippedStack.getItem(), inputCount);
 
-        return super.getCapability(cap, side);
-    }
-
-    /**
-     * reads data on world load
-     * 
-     * @param state
-     * @param nbt
-     */
-    @Override
-    public void read(BlockState state, CompoundNBT nbt) {
-        itemHandler.deserializeNBT(nbt.getCompound("inv"));
-        super.read(state, nbt);
-    }
-
-    /**
-     * writes data on world save
-     * 
-     * @param compound
-     */
-    @Override
-    public CompoundNBT write(CompoundNBT compound) {
-        compound.put("inv", itemHandler.serializeNBT());
-        return super.write(compound);
-    }
-
-    /**
-     * A function to strip logs
-     * 
-     * @param slot - slot to check
-     */
-    public void stripLogs() {
-        boolean hasUnstrippedLogInSlot = this.itemHandler.getStackInSlot(0).getCount() > 0
-                && isLog(this.itemHandler.getStackInSlot(0));
-
-        // If condition is true
-        if (hasUnstrippedLogInSlot) {
-            ItemStack is = getStrippedLog(itemHandler.getStackInSlot(0));
-
-            if (is.getItem() != null) {
-
-                // Removes the number of normal logs, replaces with same number of stripped
-                // logs.
-                this.itemHandler.insertItem(1,
-                        new ItemStack(is.getItem(),
-                                itemHandler.getStackInSlot(0).getCount()),
-                        false);
-            }
-
-        }
-
-    }
-
-    /**
-     * A function to check if the item is removed
-     * 
-     * @param slot  - slot to check
-     * @param count - number of items to remove
-     */
-    private void checkItemRemoved(int slot, int count) {
-        if (slot == 1) {
-            ItemStack logStack = this.itemHandler.getStackInSlot(0);
-            if (!logStack.isEmpty()) {
-                logStack.shrink(count);
+                itemHandler.extractItem(0, inputCount, false);
+                itemHandler.insertItem(1, strippedLogs, false);
+                markDirty();
             }
         }
+        }
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * Checks if a given itemStack is a log
-     * 
-     * @param stack to check
-     * @return true if the itemStack is a log and does not contain "stripped" in the name, false otherwise
-     */
-    public boolean isLog(ItemStack stack) {
-        return BlockTags.LOGS.contains(getBlockFromItem(stack)) && !stack.toString().contains("stripped");
-    }
+    private ItemStack getStrippedVersion(ItemStack stack) {
+        Block blockToConvert = getBlockFromItem(stack);
 
-    /**
-     * Function to check if stripped
-     * 
-     * @param stack - item stack to check
-     * @return true if contains "stripped"
-     */
-    public boolean isStrippedLog(ItemStack stack) {
-        return stack.toString().toLowerCase().contains("stripped");
-    }
+        // Checks to see if an item is a log and converts
+        if (isLog(stack)) {
+            try {
+                return createItemStack(("minecraft:stripped_" + getTypeOfLog(blockToConvert.toString()) + "_log"));
+            }
 
-    /**
-     * A function to get the block from an item
-     * 
-     * @param stack - item stack to convert
-     * @return Block
-     */
-    private static Block getBlockFromItem(ItemStack stack) {
-        return Block.getBlockFromItem(stack.getItem());
+            catch (Exception e){
+                return null;
+            }
+        }
+        return null;
     }
 
     /**
@@ -285,6 +137,15 @@ public class DebarkerTile extends TileEntity {
         return null;
     }
 
+    /**
+     * A function to get the block from an item
+     * 
+     * @param stack - item stack to convert
+     * @return Block
+     */
+    private static Block getBlockFromItem(ItemStack stack) {
+        return Block.getBlockFromItem(stack.getItem());
+    }
 
     /**
      * A function to confirm whether it has a stripped log variant
@@ -297,6 +158,7 @@ public class DebarkerTile extends TileEntity {
         }
         return false;
     }
+
 
     /**
      * Gets the type of log using a regex pattern
@@ -313,6 +175,28 @@ public class DebarkerTile extends TileEntity {
         return null;
     }
 
+    @Override
+    public void read(BlockState state, CompoundNBT compound) {
+        itemHandler.deserializeNBT(compound.getCompound("itemHandler"));
+        super.read(state, compound);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT compound) {
+        compound.put("itemHandler", itemHandler.serializeNBT());
+        return super.write(compound);
+    }
+
+    /**
+     * Checks if a given itemStack is a log
+     * 
+     * @param stack to check
+     * @return true if the itemStack is a log and does not contain "stripped" in the name, false otherwise
+     */
+    public boolean isLog(ItemStack stack) {
+        return BlockTags.LOGS.contains(getBlockFromItem(stack)) && !stack.toString().contains("stripped");
+    }
+
     /**
      * A function to create a new item stack based on a string name
      * 
@@ -325,5 +209,15 @@ public class DebarkerTile extends TileEntity {
             return new ItemStack(item);
         }
         return ItemStack.EMPTY;
+    }
+
+    /**
+     * Function to check if stripped
+     * 
+     * @param stack - item stack to check
+     * @return true if contains "stripped"
+     */
+    public boolean isStrippedLog(ItemStack stack) {
+        return stack.toString().toLowerCase().contains("stripped");
     }
 }
