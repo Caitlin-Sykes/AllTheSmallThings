899266bc53bc1c508d9fa39c0c8a3c4450cce2a8
Cait <2423179@dundee.ac.uk>
2023-08-10 19:08:11 +0100

    basic power integration

--- src/main/java/com/csykes/allthesmallthings/screen/Debarker/DebarkerScreen.java
+++ src/main/java/com/csykes/allthesmallthings/screen/Debarker/DebarkerScreen.java
@@ -2,25 +2,20 @@ package com.csykes.allthesmallthings.screen.Debarker;
 
 import com.csykes.allthesmallthings.AllTheSmallThings;
 import com.csykes.allthesmallthings.container.DebarkerContainer;
-import com.csykes.allthesmallthings.screen.Debarker.DebarkerButtonIcon;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.util.text.StringTextComponent;
 
 public class DebarkerScreen extends ContainerScreen<DebarkerContainer> {
 
     private final ResourceLocation GUI = new ResourceLocation(AllTheSmallThings.MOD_ID,
             "textures/gui/debarker_screen.png");
-
-    private Button stripLogButton;
 
     public DebarkerScreen(DebarkerContainer screen, PlayerInventory pi, ITextComponent title) {
         super(screen, pi, title);
@@ -29,11 +24,6 @@ public class DebarkerScreen extends ContainerScreen<DebarkerContainer> {
      @Override
     protected void init() {
         super.init();
-        // Adds the button
-        stripLogButton = addButton(new DebarkerButtonIcon(this.guiLeft+73, this.guiTop+30, 18, 18, button -> {
-            System.out.println("Button clicked!");
-        
-        }));
     }
 
     @Override
@@ -49,16 +39,5 @@ public class DebarkerScreen extends ContainerScreen<DebarkerContainer> {
         RenderSystem.color4f(1f, 1f, 1f, 1f);
         this.minecraft.getTextureManager().bindTexture(GUI);
         this.blit(ms, this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
-    }
-
-    /**
-     * On Button Click Handler
-     */
-    @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        if (stripLogButton.mouseClicked(mouseX, mouseY, button)) {
-            return true;
-        }
-        return super.mouseClicked(mouseX, mouseY, button);
     }
 }
