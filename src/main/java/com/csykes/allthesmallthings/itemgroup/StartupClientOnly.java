4003a4b967fe7bb14118d58fbff04616601bb1f8
Cait <2423179@dundee.ac.uk>
2023-08-31 14:42:31 +0100

    Removed unecessary imports

--- src/main/java/com/csykes/allthesmallthings/itemgroup/StartupClientOnly.java
+++ src/main/java/com/csykes/allthesmallthings/itemgroup/StartupClientOnly.java
@@ -1,7 +1,5 @@
 package com.csykes.allthesmallthings.itemgroup;
 
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
