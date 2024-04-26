0dce84b3b6d7104d7dd173afc65fa79a93a9dd11
Adam Mathieson <adam@mathieson.dev>
2024-04-27 00:26:22 +0100

    + Make More Better

--- src/main/java/com/csykes/allthesmallthings/blockEntities/DebarkerBlockEntity.java
+++ src/main/java/com/csykes/allthesmallthings/blockEntities/DebarkerBlockEntity.java
@@ -2,12 +2,17 @@ package com.csykes.allthesmallthings.blockEntities;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.Containers;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -15,6 +20,25 @@ import net.minecraftforge.energy.IEnergyStorage;
 
 public class DebarkerBlockEntity extends BlockEntity implements IEnergyStorage, ICapabilityProvider {
     private int energy;
+    private final ItemStackHandler itemHandler = new ItemStackHandler(3) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+            if(!level.isClientSide()) {
+//                ModMessages.sendToClients(new ItemStackSyncS2CPacket(this, worldPosition)); //TODO idk might need it
+            }
+        }
+
+        @Override
+        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+            return switch (slot) {
+                case 1 -> true;
+                case 2 -> false;
+                default -> super.isItemValid(slot, stack);
+            };
+        }
+    };
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.of(() -> itemHandler);
     private static final int MAX_ENERGY = 10000;
 
     public DebarkerBlockEntity(BlockPos blockPos, BlockState blockState) {
@@ -58,22 +82,34 @@ public class DebarkerBlockEntity extends BlockEntity implements IEnergyStorage, 
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+            return lazyItemHandler.cast();
         if (cap == CapabilityEnergy.ENERGY) {
             return LazyOptional.of(() -> this).cast();
         }
         return super.getCapability(cap, side);
     }
 
-
     @Override
     public void invalidateCaps() {
         super.invalidateCaps();
-//        energyStorage.invalidate();
+        lazyItemHandler.invalidate();
     }
 
     @Override
     public void reviveCaps() {
         super.reviveCaps();
-//        energyStorage.revive();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
     }
+
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        assert this.level != null;
+        Containers.dropContents(this.level, this.worldPosition, inventory);
+    }
+
 }
